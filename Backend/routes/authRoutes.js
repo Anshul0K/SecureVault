11d88778644e4c32306ead7b2c3a76577bf76e0d
Anshul <146592303+Anshul0K@users.noreply.github.com>
2025-07-25 19:03:04 +0530

    Add login autharization

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router(); 
 const { register, login } = require('../controllers/authController');
+const { protect, authorize } = require("../middlewares/authMiddleware");
 
-router.post('/register', register);
+router.post('/register', protect, authorize(['admin']), register);
+
 router.post('/login', login);
 
 module.exports = router;
